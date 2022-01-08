88d7e7c888a1695697d6e0936d703a5a42623d52
Jeff Csicsek <jcsicsek@gmail.com>
2022-01-08 13:53:13 -0500

    port from env

--- index.js
+++ index.js
@@ -37,14 +37,14 @@ const url = oauth2Client.generateAuthUrl({
   redirect_uri: "google.com",
 });
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
